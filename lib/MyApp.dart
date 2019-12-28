e0cb75b962164a3ff7859a4cdc283511452418c9
erikrios <erikriosetiawan49@gmail.com>
2019-12-29 00:42:36 +0700

    Add the example code of radio list tile

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:radio_and_radio_list_tile/RadioHomePage.dart';
+import 'package:radio_and_radio_list_tile/RadioListTileHomePage.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Demo Flutter',
         theme: ThemeData(primarySwatch: Colors.green),
-        home: RadioHomePage(),
+        home: RadioListTileHomePage(),
       );
 }
---
+++ lib/RadioListTileHomePage.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class RadioListTileHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RadioListTileHomePageState();
+}
+
+class _RadioListTileHomePageState extends State<RadioListTileHomePage> {
+  int selected = -1;
+
+  void onChanged(int value) {
+    setState(() {
+      this.selected = value;
+      print('Pilihan: ${this.selected}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Demo Radio List Tile'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text('Jenis Kelamin:'),
+              RadioListTile(
+                value: 0,
+                groupValue: this.selected,
+                onChanged: (int value) {
+                  onChanged(value);
+                },
+                title: Text('Pria'),
+                activeColor: Colors.red,
+                secondary: Icon(Icons.group),
+              ),
+              RadioListTile(
+                value: 1,
+                groupValue: (int value) {
+                  onChanged(value);
+                },
+                title: Text('Wanita'),
+                activeColor: Colors.red,
+                secondary: Icon(Icons.group),
+              )
+            ],
+          ),
+        ),
+      );
+}
